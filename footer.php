9173c740c5e9d07e8acaa0e6d7c91eb5a2d7fd4d
emmachenn <92216351+emmachenn@users.noreply.github.com>
2021-12-15 17:23:21 +0800

    网页脚，后台系统入口
    方便管理员进入后台系统处理后台消息

---
+++ footer.php
@@ -0,0 +1,6 @@
+//网页脚，后台系统入口
+
+
+<div class="wrapper">
+  <div class="footer"> CopyRight &copy; <?php echo date('Y')?> <?php echo WEB_NAME ?> , All Rights Reserved <a href="admin/index.php">【后台管理】</a> </div>
+</div>
